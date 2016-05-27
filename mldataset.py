dc07be5316577b126f8ba7eff916922cbf5a9a2c
Pradeep Reddy Raamana <raamana@gmail.com>
2016-05-27 00:27:43 -0400

    - copy constructor from another dataset - enabling setters for key attributes - methods to remove samples, substract another dataset and extend by another dataset - more coherent examples to go with improved class methods

--- mldataset.py
+++ mldataset.py
@@ -4,26 +4,34 @@ from itertools import ifilter, takewhile, islice
 import warnings
 import os
 import cPickle as pickle
+import copy
 
 class MLDataset(object):
     """Class defining a ML dataset that helps maintain integrity and ease of access."""
 
-    def __init__(self, filepath=None, data=None, labels=None, classes=None, description=''):
+    def __init__(self, filepath=None, in_dataset=None, data=None, labels=None, classes=None, description=''):
         """Default constructor. Suggested way to construct the dataset is via add_sample method."""
 
-        if filepath is not None and os.path.isfile(filepath):
-            self.__load(filepath)
-        elif data == labels == classes == None:
+        if filepath is not None:
+            if os.path.isfile(filepath):
+                print 'Loading the dataset from: {}'.format(filepath)
+                self.__load(filepath)
+            else:
+                raise IOError('Specified file could not be read.')
+        elif in_dataset is not None and isinstance(in_dataset, MLDataset):
+            assert in_dataset, ValueError('Dataset to copy is empty.')
+            self.__copy(in_dataset)
+        elif data is None and labels is None and classes is None:
             self.__data = OrderedDict()
             self.__labels = OrderedDict()
             self.__classes = OrderedDict()
             self.__num_features = 0
             self.__description = description
         elif data is not None and labels is not None and classes is not None:
-            assert isinstance(data, dict), 'data must be a dict! keys: subject ID or any unique identifier'
-            assert isinstance(labels, dict), 'labels must be a dict! keys: subject ID or any unique identifier'
+            assert isinstance(data, dict), 'data must be a dict! keys: sample ID or any unique identifier'
+            assert isinstance(labels, dict), 'labels must be a dict! keys: sample ID or any unique identifier'
             if classes is not None:
-                assert isinstance(classes, dict), 'labels must be a dict! keys: subject ID or any unique identifier'
+                assert isinstance(classes, dict), 'labels must be a dict! keys: sample ID or any unique identifier'
 
             assert len(data) == len(labels) == len(classes), 'Lengths of data, labels and classes do not match!'
             assert set(data.keys()) == set(labels.keys()) == set(classes.keys()), 'data, classes and labels ' \
@@ -50,7 +58,7 @@ class MLDataset(object):
     def data_matrix(self):
         """dataset features in a matrix form."""
         mat = np.zeros([self.num_samples, self.num_features])
-        for ix, (sub, features) in enumerate(self.__data.items()):
+        for ix, (sample, features) in enumerate(self.__data.items()):
             mat[ix, :] = features
         return mat
 
@@ -70,12 +78,13 @@ class MLDataset(object):
     @property
     def labels(self):
         """Returns the array of labels for all the samples."""
-        return self.__labels.values()
+        #TODO numeric label need to be removed, as this can be made up on the fly as needed from str to num encoders.
+        return self.__labels
 
     @property
     def target(self):
         """Returns the array of labels for all the samples."""
-        return self.labels
+        return self.__labels.values()
 
     @labels.setter
     def labels(self, values):
@@ -88,10 +97,30 @@ class MLDataset(object):
         else:
             raise ValueError('labels input must be a dictionary!')
 
+
+    @property
+    def classes(self):
+        """Identifiers (sample IDs, or sample names etc) forming the basis of dict-type MLDataset."""
+        return self.__classes
+
+    @classes.setter
+    def classes(self, values):
+        """Classes setter."""
+        if isinstance(values, dict):
+            if self.__data is not None and len(self.__data) != len(values):
+                raise ValueError('number of samples do not match the previously assigned data')
+            elif set(self.keys) != set(values.keys()):
+                raise ValueError('sample ids do not match the previously assigned ids.')
+            else:
+                self.__classes = values
+        else:
+            raise ValueError('classes input must be a dictionary!')
+
+
     @property
     def class_sizes(self):
         """Returns the sizes of different objects in a Counter object."""
-        return Counter(self.classes)
+        return Counter(self.classes.values())
 
     @property
     def __label_set(self):
@@ -107,14 +136,14 @@ class MLDataset(object):
         return self.__take(nitems, self.__data.iteritems())
 
 
-    def add_sample(self, subject_id, features, label, class_id=None):
+    def add_sample(self, sample_id, features, label, class_id=None):
         """Adds a new sample to the dataset with its features, label and class ID.
         This is the preferred way to construct the dataset."""
-        if subject_id not in self.__data:
+        if sample_id not in self.__data:
             if self.num_samples <= 0:
-                self.__data[subject_id] = features
-                self.__labels[subject_id] = label
-                self.__classes[subject_id] = class_id
+                self.__data[sample_id] = features
+                self.__labels[sample_id] = label
+                self.__classes[sample_id] = class_id
                 self.__dtype = type(features)
                 self.__num_features = len(features)
             else:
@@ -123,11 +152,21 @@ class MLDataset(object):
                     len(features),self.__num_features))
                 assert isinstance(features,self.__dtype), TypeError("Mismatched dtype. Provide {}".format(self.__dtype))
 
-                self.__data[subject_id] = features
-                self.__labels[subject_id] = label
-                self.__classes[subject_id] = class_id
+                self.__data[sample_id] = features
+                self.__labels[sample_id] = label
+                self.__classes[sample_id] = class_id
         else:
-            raise ValueError('{} already exists in this dataset!'.format(subject_id))
+            raise ValueError('{} already exists in this dataset!'.format(sample_id))
+
+    def del_sample(self, sample_id):
+        """Method remove a sample from the dataset."""
+        if sample_id not in self.__data:
+            warnings.warn('Sample to delete not found in the dataset - nothing to do.')
+        else:
+            self.__data.pop(sample_id)
+            self.__classes.pop(sample_id)
+            self.__labels.pop(sample_id)
+            print '{} removed.'.format(sample_id)
 
     def get_feature_subset(self, subset_idx):
         """Returns the subset of features indexed numerically. """
@@ -136,7 +175,7 @@ class MLDataset(object):
         assert ( max(subset_idx) < self.__num_features) and (min(subset_idx)>=0), \
             UnboundLocalError('indices out of range for the dataset. Max index: {}'.format(self.__num_features))
 
-        sub_data = {sub : features[subset_idx] for sub, features in self.__data.items()}
+        sub_data = {sample : features[subset_idx] for sample, features in self.__data.items()}
         new_descr = 'Subset features derived from: \n ' + self.__description
         subdataset = MLDataset(data=sub_data, labels=self.__labels, classes=self.__classes, description=new_descr)
 
@@ -154,7 +193,7 @@ class MLDataset(object):
         subsets = list()
         for class_id in class_ids:
             if class_id in self.class_set:
-                subsets_this_class = [sub_id for sub_id in self.__classes if self.__classes[sub_id] == class_id]
+                subsets_this_class = [sample for sample in self.__classes if self.__classes[sample] == class_id]
                 subsets.extend(subsets_this_class)
             else:
                 warnings.warn('Requested class: {} does not exist in this dataset.'.format(class_id))
@@ -166,17 +205,17 @@ class MLDataset(object):
             return self.get_subset(subsets)
 
     def get_subset(self, subset_ids):
-        """Returns a smaller dataset identified by their keys/subject IDs."""
+        """Returns a smaller dataset identified by their keys/sample IDs."""
         num_existing_keys = sum([1 for key in subset_ids if key in self.__data])
         if subset_ids is not None and num_existing_keys > 0:
-            # need to ensure data are added to data, labels etc in the same order of subject IDs
+            # need to ensure data are added to data, labels etc in the same order of sample IDs
             data = self.__get_subset_from_dict(self.__data, subset_ids)
             labels = self.__get_subset_from_dict(self.__labels, subset_ids)
             if self.__classes is not None:
                 classes = self.__get_subset_from_dict(self.__classes, subset_ids)
             else:
                 classes = None
-            subdataset = MLDataset(data, labels, classes)
+            subdataset = MLDataset(data=data, labels=labels, classes=classes)
             # Appending the history
             subdataset.description += '\n Subset derived from: ' + self.description
             return subdataset
@@ -184,26 +223,19 @@ class MLDataset(object):
             warnings.warn('subset of IDs requested do not exist in the dataset!')
             return MLDataset()
 
-    def __get_subset_from_dict(self, input_dict, subset):
-        # Using OrderedDict helps ensure data are added to data, labels etc in the same order of subject IDs
+    @staticmethod
+    def __get_subset_from_dict(input_dict, subset):
+        # Using OrderedDict helps ensure data are added to data, labels etc in the same order of sample IDs
         return OrderedDict((sid, value) for sid, value in input_dict.items() if sid in subset)
-
-        # # statement below doesn't work for some reason
-        # return OrderedDict(ifilter( lambda key: key in subset, input_dict))
 
     @property
     def keys(self):
-        """Identifiers (subject IDs, or sample names etc) forming the basis of dict-type MLDataset."""
+        """Identifiers (sample IDs, or sample names etc) forming the basis of dict-type MLDataset."""
         return self.__data.keys()
 
     @property
-    def subject_ids(self):
+    def sample_ids(self):
         return self.keys
-
-    @property
-    def classes(self):
-        """Identifiers (subject IDs, or sample names etc) forming the basis of dict-type MLDataset."""
-        return self.__classes.values()
 
     @property
     def description(self):
@@ -220,6 +252,21 @@ class MLDataset(object):
     def num_features(self):
         """number of features in each sample."""
         return self.__num_features
+
+    @num_features.setter
+    def num_features(self, int_val):
+        assert isinstance(int_val, int) and (0 < int_val < np.Inf), UnboundLocalError('Invalid number of features.')
+        self.__num_features = int_val
+
+    @property
+    def dtype(self):
+        """number of features in each sample."""
+        return self.__dtype
+
+    @dtype.setter
+    def dtype(self, type_val):
+        #TODO assert isinstance(type_val,type), TypeError('Invalid data type.')
+        self.__dtype = type_val
 
     @property
     def num_samples(self):
@@ -265,10 +312,12 @@ class MLDataset(object):
             max_width = max([len(cls) for cls in class_ids])
             for cls in class_ids:
                 full_descr.append('Class {:>{}} : {} samples.'.format(cls, max_width, self.class_sizes.get(cls)))
+        else:
+            full_descr.append('Empty dataset.')
 
         return '\n'.join(full_descr)
 
-    def __format__(self, fmt_str):
+    def __format__(self, fmt_str='s'):
         if isinstance(fmt_str, basestring):
             return '{} samples x {} features with {} classes'.format(
                 self.num_samples, self.num_features, self.num_classes)
@@ -287,35 +336,49 @@ class MLDataset(object):
                 'classes',
                 'data',
                 'data_matrix',
+                'del_sample',
                 'description',
+                'extend',
                 'get_class',
                 'get_subset',
                 'get_feature_subset',
                 'keys',
-                'labels',
+                'target',
                 'num_classes',
                 'num_features',
                 'num_samples',
-                'subject_ids',
+                'sample_ids',
+                'save',
                 'add_classes' ]
 
+    def __copy(self, other):
+        """Copy constructor."""
+        self.__data, self.__classes, self.__labels, self.__dtype, self.__description, self.__num_features = \
+            other.data, other.classes, other.labels, other.dtype, other.description, other.num_features
+
+        return self
+
     def __load(self, path):
+        """Method to load the serialized dataset from disk."""
         try:
             path = os.path.abspath(path)
             with open(path, 'rb') as df:
                 # loaded_dataset = pickle.load(df)
-                self.__data, self.__classes, self.__labels, self.__dtype, self.__description, self.__num_features = pickle.load(df)
+                self.__data, self.__classes, self.__labels, \
+                    self.__dtype, self.__description, self.__num_features = pickle.load(df)
         except IOError as ioe:
             raise IOError('Unable to read the dataset from file: {}',format(ioe))
         except:
             raise
 
     def save(self, path):
+        """Method to serialize the dataset to disk."""
         try:
             path = os.path.abspath(path)
             with open(path, 'wb') as df:
                 # pickle.dump(self, df)
-                pickle.dump((self.__data, self.__classes, self.__labels, self.__dtype, self.__description, self.__num_features),
+                pickle.dump((self.__data, self.__classes, self.__labels,
+                             self.__dtype, self.__description, self.__num_features),
                             df)
             return
         except IOError as ioe:
@@ -323,3 +386,73 @@ class MLDataset(object):
         except:
             raise
 
+    def extend(self, other):
+        """Method to extend the dataset vertically (add samples from  anotehr dataset)."""
+        assert isinstance(other, MLDataset), TypeError('Incorrect type of dataset provided!')
+        assert self.__dtype==other.dtype, TypeError('Incorrect data type of features!')
+        for sample in other.keys:
+            self.add_sample(sample, other.data[sample], other.labels[sample], other.classes[sample])
+
+    def __add__(self, other):
+        assert isinstance(other,MLDataset), TypeError('Incorrect type of dataset provided!')
+
+        if set(self.keys) == set(other.keys):
+            print 'Identical keys found. Trying to horizontally concatenate features for each sample.'
+            assert self.__classes == other.classes, ValueError('Class identifiers per sample differ in the two '
+                                                                 'datasets!')
+            assert other.num_features > 0, ValueError('No features to concatenate.')
+            # making an empty dataset
+            combined = MLDataset()
+            # populating it with the concatenated feature set
+            for sample in self.keys:
+                comb_data = np.concatenate([self.__data[sample], other.data[sample]])
+                combined.add_sample(sample, comb_data, self.__labels[sample], self.__classes[sample])
+
+            return combined
+
+        elif len(set(self.keys).intersection(other.keys)) < 1 and self.__num_features == other.num_features:
+            # making a copy of self first
+            combined = MLDataset(in_dataset=self)
+            # adding the new dataset
+            combined.extend(other)
+            return combined
+        else:
+            raise ArithmeticError('Two datasets could not be combined.')
+
+
+    def __sub__(self, other):
+        assert isinstance(other, type(self)), TypeError('Incorrect type of dataset provided!')
+
+        num_existing_keys = len(set(self.keys).intersection(other.keys))
+        if num_existing_keys < 1:
+            warnings.warn('None of the keys in the dataset to be removed found in this dataset - nothing to do.')
+        if len(self.keys) == num_existing_keys:
+            warnings.warn('Requested removal of all the samples - output dataset would be empty.')
+
+        removed = copy.deepcopy(self)
+        for sample in other.keys:
+            removed.del_sample(sample)
+
+        return removed
+
+    def __iadd__(self, other):
+        """Augmented assignment for add."""
+        return self.__add__(other)
+
+    def __isub__(self, other):
+        """Augmented assignment for sample."""
+        return self.__sub__(other)
+
+    def __eq__(self, other):
+        """Equality of two datasets."""
+        if set(self.keys) != set(other.keys):
+            print 'differing sample ids.'
+            return False
+        elif self.__data != other.data:
+            print 'differing data for the sample ids.'
+            return False
+        elif self.__classes != other.classes:
+            print 'differing classes for the sample ids.'
+            return False
+        else:
+            return True
