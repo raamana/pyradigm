a5e88b0434d5933443ba10f8908e19cefa3ad4d8
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-09-06 18:18:06 -0400

    helping RTD builds with numpy C requirements [skip ci]

--- conf.py
+++ conf.py
@@ -23,6 +23,16 @@ sys.path.insert(0, os.path.abspath('./pyradigm'))
 
 import sphinx_rtd_theme
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = [ 'argparse', 'numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
