1f2b1a61c7ef5ade8dc9a2f4349123b7a7c34d32
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-09-06 18:26:46 -0400

    still fixing RTD builds [skip ci]

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = [ 'argparse', 'numpy']
+MOCK_MODULES = [ 'argparse', 'numpy', 'numpydoc']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
