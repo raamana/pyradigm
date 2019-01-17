0e092aae372cf04a06427166688b47135b1dea13
Pradeep Reddy Raamana <raamana@gmail.com>
2019-01-16 22:49:28 -0500

    rudimentary tests

--- pyradigm/__init__.py
+++ pyradigm/__init__.py
@@ -1,11 +1,13 @@
-__all__ = [ 'pyradigm', 'MLDataset', 'cli_run', 'check_compatibility' ]
+__all__ = [ 'pyradigm', 'MLDataset', 'MultiDataset', 'cli_run', 'check_compatibility' ]
 
 from sys import version_info
 
 if version_info.major==2 and version_info.minor==7:
     from pyradigm import MLDataset, cli_run, check_compatibility
+    from multiple import MultiDataset
 elif version_info.major > 2:
     from pyradigm.pyradigm import MLDataset, cli_run, check_compatibility
+    from pyradigm.multiple import MultiDataset
 else:
     raise NotImplementedError('pyradigm supports only 2.7 or 3+. '
                               'Upgrade to Python 3+ is recommended.')
