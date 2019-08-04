36f58b740b695b2ceb1dd64cff8cf44b9161b5bb
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-04 10:36:40 -0400

    cleaning up import setup; python 2 support dropped

--- pyradigm/__init__.py
+++ pyradigm/__init__.py
@@ -1,19 +1,20 @@
-__all__ = [ 'pyradigm', 'MLDataset', 'MultiDataset', 'cli_run',
-            'check_compatibility_BaseDataset']
+
+__all__ = [ 'ClassificationDataset', 'RegressionDataset', 'MultiDataset',
+            'BaseDataset', 'check_compatibility_BaseDataset',
+            'pyradigm', 'MLDataset', 'cli_run']
 
 from sys import version_info
 
-if version_info.major==2:
-    raise NotImplementedError()
-elif version_info.major > 2:
+
+if version_info.major >= 3:
     from pyradigm.base import BaseDataset, check_compatibility_BaseDataset
     from pyradigm.classify import ClassificationDataset
+    from pyradigm.regress import RegressionDataset
     from pyradigm.pyradigm import MLDataset, cli_run
     from pyradigm.multiple import MultiDataset
 else:
-    raise NotImplementedError('pyradigm supports only 2.7 or 3+. '
+    raise NotImplementedError('pyradigm supports only Python 3 or higher! '
                               'Upgrade to Python 3+ is recommended.')
-
 
 
 from ._version import get_versions
