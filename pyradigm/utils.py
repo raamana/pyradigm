56687b72ec1be4c26f341dd2ffff2e48187b97ec
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-05 10:56:06 -0400

    stupid bug fix

--- pyradigm/utils.py
+++ pyradigm/utils.py
@@ -1,22 +1,22 @@
-
-from pyradigm.pyradigm import MLDataset
-from pyradigm import ClassificationDataset
 import numpy as np
+from pyradigm import ClassificationDataset
+from pyradigm.pyradigm import MLDataset
 
 feat_generator = np.random.randn
 
-def make_random_dataset(max_num_classes = 20,
-                          min_class_size = 20,
-                          max_class_size = 50,
-                          max_dim = 100,
-                          stratified = True,
-                          class_type=ClassificationDataset):
+
+def make_random_dataset(max_num_classes=20,
+                        min_class_size=20,
+                        max_class_size=50,
+                        max_dim=100,
+                        stratified=True,
+                        class_type=ClassificationDataset):
     "Generates a random Dataset for use in testing."
 
     smallest = min(min_class_size, max_class_size)
     max_class_size = max(min_class_size, max_class_size)
     largest = max(50, max_class_size)
-    largest = max(smallest+3,largest)
+    largest = max(smallest + 3, largest)
 
     if max_num_classes != 2:
         num_classes = np.random.randint(2, max_num_classes, 1)
@@ -41,7 +41,7 @@ def make_random_dataset(max_num_classes = 20,
 
     ds = class_type()
     for cc, class_ in enumerate(class_ids):
-        subids = [ 's{}-c{}'.format(ix,cc) for ix in range(class_sizes[cc]) ]
+        subids = ['s{}-c{}'.format(ix, cc) for ix in range(class_sizes[cc])]
         for sid in subids:
             if isinstance(ds, MLDataset):
                 ds.add_sample(sid, feat_generator(num_features), int(cc), class_)
@@ -51,31 +51,31 @@ def make_random_dataset(max_num_classes = 20,
     return ds
 
 
-def make_random_ClfDataset(max_num_classes = 20,
-                          min_class_size = 20,
-                          max_class_size = 50,
-                          max_dim = 100,
-                          stratified = True):
+def make_random_ClfDataset(max_num_classes=20,
+                           min_class_size=20,
+                           max_class_size=50,
+                           max_dim=100,
+                           stratified=True):
     "Generates a random ClassificationDataset for use in testing."
 
-    make_random_dataset(max_num_classes = max_num_classes,
-                          min_class_size = min_class_size,
-                          max_class_size = max_class_size,
-                          max_dim = max_dim,
-                          stratified = stratified,
-                          class_type=ClassificationDataset)
+    return make_random_dataset(max_num_classes=max_num_classes,
+                               min_class_size=min_class_size,
+                               max_class_size=max_class_size,
+                               max_dim=max_dim,
+                               stratified=stratified,
+                               class_type=ClassificationDataset)
 
 
-def make_random_MLdataset(max_num_classes = 20,
-                          min_class_size = 20,
-                          max_class_size = 50,
-                          max_dim = 100,
-                          stratified = True):
+def make_random_MLdataset(max_num_classes=20,
+                          min_class_size=20,
+                          max_class_size=50,
+                          max_dim=100,
+                          stratified=True):
     "Generates a random MLDataset for use in testing."
 
-    make_random_dataset(max_num_classes = max_num_classes,
-                          min_class_size = min_class_size,
-                          max_class_size = max_class_size,
-                          max_dim = max_dim,
-                          stratified = stratified,
-                          class_type=MLDataset)
+    return make_random_dataset(max_num_classes=max_num_classes,
+                               min_class_size=min_class_size,
+                               max_class_size=max_class_size,
+                               max_dim=max_dim,
+                               stratified=stratified,
+                               class_type=MLDataset)
