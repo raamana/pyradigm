3b9d2406956e4a1b4cfaccf3d471ffbf6b1ca493
Pradeep Reddy Raamana <raamana@gmail.com>
2019-08-08 19:35:01 -0400

    thorough equality comparison between original and reloaded

--- pyradigm/tests/test_common_behaviours.py
+++ pyradigm/tests/test_common_behaviours.py
@@ -13,15 +13,40 @@ from inspect import signature
 from pyradigm import (ClassificationDataset as ClfDataset,
                       RegressionDataset as RegrDataset)
 from pyradigm.utils import make_random_ClfDataset
+from pyradigm.base import is_iterable_but_not_str
 from pytest import raises, warns
 import numpy as np
 import random
+import os
+from os.path import join as pjoin, realpath, dirname
+from warnings import warn
 
 class_list = (ClfDataset, RegrDataset)
 
 constructor_must_offer_param_list = ('dataset_path',
                                      'data', 'targets', 'dtype',
                                      'description', 'feature_names')
+
+cur_dir = dirname(realpath(__file__))
+out_dir = pjoin(cur_dir, 'tmp')
+os.makedirs(out_dir, exist_ok=True)
+out_file = pjoin(out_dir, 'random_example_dataset.pkl')
+
+ds = make_random_ClfDataset()
+
+
+def _not_equal(array_one, array_two):
+
+    if is_iterable_but_not_str(array_one):
+        not_equal = any(np.not_equal(array_one, array_two))
+    else:
+        not_equal = array_one != array_two
+
+    if not isinstance(not_equal, bool):
+        not_equal = any(not_equal)
+
+    return not_equal
+
 
 
 def test_constructor_must_offer_params():
@@ -36,7 +61,6 @@ def test_constructor_must_offer_params():
 def test_attributes():
     """Creation, access and properties"""
 
-    ds = make_random_ClfDataset()
     id_list = np.array(ds.samplet_ids)
 
     # ensuring strings can't be added to float attributes
@@ -93,4 +117,58 @@ def test_attributes():
         raise AttributeError('Attr deletion failed!')
 
 
-test_attributes()
+def test_save_load():
+
+    ds.save(file_path=out_file)
+    reloaded_ds = ds.__class__(dataset_path=out_file)
+
+    if ds != reloaded_ds:
+        raise IOError('Error in save/load implementation!')
+
+    must_have_attr = ('_data', '_targets',
+                      '_dtype', '_target_type', '_description',
+                      '_num_features', '_feature_names',
+                      '_attr', '_attr_dtype', '_dataset_attr')
+
+    for attr in must_have_attr:
+        if not hasattr(reloaded_ds, attr):
+            raise AttributeError('Attribute {} missing after reload from disk'
+                                 ''.format(attr))
+
+        orig_val = getattr(ds, attr)
+        reloaded = getattr(reloaded_ds, attr)
+
+        not_equal = False
+        try:
+            if isinstance(orig_val, dict):
+                for key, val in orig_val.items():
+                    if _not_equal(val, reloaded[key]):
+                        warn('Values differ for attr {} in samplet {}'
+                                         ' when reloaded from disk'.format(attr, key))
+                        not_equal = True
+                        break
+            elif is_iterable_but_not_str(orig_val):
+                for aa, bb in zip(orig_val, reloaded):
+                    if aa != bb:
+                        not_equal= True
+                        break
+                # not_equal = any(np.not_equal(orig_val, reloaded))
+            elif np.issubdtype(type(orig_val), np.generic):
+                not_equal = reloaded != orig_val
+            else:
+                raise TypeError('Unrecognized type {} for attr {}'
+                                ''.format(type(orig_val), attr))
+        except:
+            raise
+
+        if not isinstance(not_equal, bool):
+            not_equal = any(not_equal)
+
+        if not_equal:
+            raise AttributeError('Attribute {} differs between the reloaded'
+                                 ' and the original datasets'.format(attr))
+
+
+# test_attributes()
+test_save_load()
+# os.rmdir(out_dir)
