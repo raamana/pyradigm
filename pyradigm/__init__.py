91eddd875dd59ec2c7d28180b6be75d4b4f413e8
Pradeep Reddy Raamana <raamana@gmail.com>
2023-03-27 13:32:38 -0400

    updating init

--- pyradigm/__init__.py
+++ pyradigm/__init__.py
@@ -15,7 +15,7 @@ else:
     raise NotImplementedError('pyradigm supports only Python 3 or higher! '
                               'Upgrade to Python 3+ is recommended.')
 
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
